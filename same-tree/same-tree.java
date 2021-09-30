f734db368c770461b0e2a894a4e60c74fc0f4808
shannonbonet <63089231+shannonbonet@users.noreply.github.com>
2021-09-30 15:02:32 -0700

    Time: 0 ms (100.00%), Space: 36.3 MB (81.72%) - LeetHub

--- same-tree/same-tree.java
+++ same-tree/same-tree.java
@@ -15,14 +15,25 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        // recurse down p and q 
+        // base case = same tree --> true 
+        // check if structure same --> recurse; else false 
+        // checking structure 
+            // p.right + q.right exist + have same value 
+        
         if (p == null && q == null) {
             return true; 
+        } 
+        
+        // missing base case - if one of p/q is null 
+        if (p == null || q == null) {
+            return false; 
         }
         
-        if (p != null && q != null && p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right); 
-        }
+        if (p != null && q != null && p.val != q.val) {
+            return false; 
+        } 
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right); 
         
-        return false; 
     }
 }
