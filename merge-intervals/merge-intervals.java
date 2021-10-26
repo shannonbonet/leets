0d3875d98241e5ed17f7a8f5a6e10be67554240c
shannonbonet <63089231+shannonbonet@users.noreply.github.com>
2021-10-25 22:08:55 -0700

    Time: 5 ms (96.34%), Space: 41.5 MB (89.97%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,28 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        
         //for each interval, sort by the first column 
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); 
         
         //cannot create dynamically changing Arrays 
         //frequent changes -> LinkedList; frequent retrival -> ArrayList 
-        LinkedList<int[]> list = new LinkedList<> (); 
-        for (int[] i : intervals) {
-            
-            if (list.isEmpty() || list.getLast()[1] < i[0]) {
-                list.add(i);
-            } else { //automatically starts on second item 
-                list.getLast()[1] = Math.max(i[1], list.getLast()[1]);
+        LinkedList<int[]> list = new LinkedList<>(); 
+        
+        //iterate through itervals --> merge overlapping intervals 
+        for (int[] interval : intervals) {
+            if (list.isEmpty() || list.getLast()[1] < interval[0]) {
+                list.add(interval); 
+            } else {
+                list.getLast()[1] = Math.max(list.getLast()[1], interval[1]); 
             }
-                
-            // for (int j = i + 1; j < intervals.length; j++) {
-            //     int[] b = intervals[j]; 
-            //     if (b[0] <= a[1]) {
-            //         int oldA = list.getLast()[0]; 
-            //         list.removeLast(); 
-            //         list.add(new int[]{Math.min(a[0], oldA), Math.max(a[1], b[1])});
-            //     } 
-            // }
         }
         return list.toArray(new int[list.size()][]); 
     }
