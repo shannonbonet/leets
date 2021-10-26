34b582d4813ab09a8a945b9f49c3f595fbef4448
shannonbonet <63089231+shannonbonet@users.noreply.github.com>
2021-10-25 19:04:57 -0700

    Time: 106 ms (9.11%), Space: 42.1 MB (30.57%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,17 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<> (); 
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i); //can use .get later
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i]; 
-            if (map.containsKey(complement) && i != map.get(complement)) {
-                return new int[]{i, map.get(complement)}; 
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j}; 
+                }
             }
         }
         return null; 
     }
 }
-
-//nums[i] != complement prevents duplicate element values; should prevent duplicate element indices instead 
